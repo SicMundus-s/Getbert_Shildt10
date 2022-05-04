4c4afbbff00242e1ec097e837be0925499d735cd
mrNRaven-hub <nikita.volkov0505@mail.ru>
2022-05-04 18:28:30 +0300

    Added chapter12

---
+++ src/chapter12_enumerations_autoboxing_and_annotations/Annotations.java
@@ -0,0 +1,32 @@
+package chapter12_enumerations_autoboxing_and_annotations;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface myAnno {
+    String name();
+    int val();
+    boolean lazyload() default false;
+}
+
+public class Annotations {
+    @myAnno(name = "пример аннотации", val = 100)
+    public static void testAnno () {
+        Annotations an = new Annotations();
+
+        try {
+            Class<?> c = an.getClass();
+
+            Method m = c.getMethod("MyMeth");
+
+            myAnno anno =  m.getAnnotation(myAnno.class);
+
+            System.out.println(anno.name() + " " + anno.val());
+        }catch (NoSuchMethodException exc) {
+            System.out.println("Метод не найден");
+        }
+    }
+}
---
+++ src/chapter12_enumerations_autoboxing_and_annotations/Main.java
@@ -0,0 +1,29 @@
+package chapter12_enumerations_autoboxing_and_annotations;
+
+import java.util.Scanner;
+enum Apple {
+    Jonathan(10), GoldenDel(15), RedDel(20), Winesap(25), Cortland(30);
+    private int price;
+    Apple(int p) {price = p;}
+    public int getPrice() {return price;}
+
+}
+
+public class Main {
+    public static void main(String args[]) {
+
+        int i = 0;
+        Integer im = 100;
+        i = im;
+        System.out.println(i);
+        Apple ap;
+        ap = Apple.valueOf("GoldenDel");
+
+        for(Apple a : Apple.values()) {
+            System.out.println(a + "стои " + a.getPrice() + " юаний");
+        }
+
+        System.out.println(ap);
+    }
+}
+
