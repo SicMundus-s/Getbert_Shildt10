2032608b003b86c1b6b2095fa65c5708bfa0046c
NikitaV <nikita.volkov0505@mail.ru>
2022-07-28 11:59:40 +0300

    Stream API: FindFirst/any, allMatch/anyMatch/noneMatch, min/max

--- Part_3/Java8-16/src/StreamAPI/Main.java
+++ Part_3/Java8-16/src/StreamAPI/Main.java
@@ -1,9 +1,6 @@
 package StreamAPI;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.IntPredicate;
 import java.util.stream.Stream;
 
@@ -48,6 +45,67 @@ public class Main {
                 .forEach(s->System.out.println(s));
 
 
-        
+        /**
+         * count - возвращает кол-во элементов в массиве
+         */
+        ArrayList<String> names = new ArrayList<String>();
+        names.addAll(Arrays.asList(new String[]{"Tom", "Sam", "Bob", "Alice"}));
+        System.out.println(names.stream().count());  // 4
+
+        // количество элементов с длиной не больше 3 символов
+        System.out.println(names.stream().filter(n->n.length()<=3).count());  // 3
+
+        /**
+         * Метод findFirst() извлекает из потока первый элемент, а findAny() извлекает случайный объект из потока (нередко так же первый):
+         */
+
+        ArrayList<String> names1 = new ArrayList<>();
+        names.addAll(Arrays.asList("Tom", "Sam", "Bob", "Alice"));
+
+        Optional<String> first = names1.stream().findFirst();
+        System.out.println(first.get());    // Tom
+
+        Optional<String> any = names1.stream().findAny();
+        System.out.println(any.get());    // Tom
+
+        /**
+         * boolean allMatch(Predicate<? super T> predicate): возвращает true, если все элементы потока удовлетворяют условию в предикате
+         *
+         * boolean anyMatch(Predicate<? super T> predicate): возвращает true, если хоть один элемент потока удовлетворяют условию в предикате
+         *
+         * boolean noneMatch(Predicate<? super T> predicate): возвращает true, если ни один из элементов в потоке не удовлетворяет условию в предикате
+         */
+
+        ArrayList<String> names2 = new ArrayList<>();
+        names.addAll(Arrays.asList("Tom", "Sam", "Bob", "Alice"));
+
+        // есть ли в потоке строка, длина которой больше 3
+        boolean any1 = names2.stream().anyMatch(s->s.length()>3);
+        System.out.println(any1);    // true
+
+        // все ли строки имеют длину в 3 символа
+        boolean all = names2.stream().allMatch(s->s.length()==3);
+        System.out.println(all);    // false
+
+        // НЕТ ЛИ в потоке строки "Bill". Если нет, то true, если есть, то false
+        boolean none = names2.stream().noneMatch(s->s=="Bill");
+        System.out.println(none);   // true
+
+        /**
+         * Методы min() и max() возвращают соответственно минимальное и максимальное значение
+         * Optional<T> min(Comparator<? super T> comparator)
+         * Optional<T> max(Comparator<? super T> comparator)
+         */
+
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+        Optional<Integer> min = numbers.stream().min(Integer::compare);
+        Optional<Integer> max = numbers.stream().max(Integer::compare);
+        System.out.println(min.get());  // 1
+        System.out.println(max.get());  // 9
+
+        /**
+         *
+         */
     }
 }
